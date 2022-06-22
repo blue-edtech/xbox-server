f74478c8f151911df057ef10e8e762876d618b84
Leo Ruiz <ruizruizleo@hotmail.com>
2022-06-22 19:34:21 -0300

    chore: JWT sign generate token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
   async login(loginDto: LoginDto): Promise<LoginResponseDto> {
+    const { email, password } = loginDto;
+
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      throw new UnauthorizedException('User and/or password is invalid');
+    }
+    //const isHashValid = await bcrypt.compare(password, user.password);
+    // if (!isHashValid) {
+    //   throw new UnauthorizedException('Usuário e/ou senha inválidos');
+    // }
+
+    delete user.password;
+
     return {
-      token: 'Teste',
-      user: undefined,
+      token: this.jwtService.sign({ email }),
+      user,
     };
   }
 }
